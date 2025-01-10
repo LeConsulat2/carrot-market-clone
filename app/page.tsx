f1a2b36a5a18dc7d465dbc77c4d08a2feb7f0721
LeConsulat2 <wooine2@gmail.com>
2025-01-10 14:40:32 +1300

    (person, index) map and coloring

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,24 @@
 export default function Home() {
   return (
     <main className="bg-gray-300  sm:bg-red-200 md:bg-green-200 lg:bg-cyan-300 xl:bg-slate-950 h-screen flex items-center justify-center p-5 ">
-      <div className="bg-white w-full flex flex-col flex-1 md:flex-row gap-2 shadow-lg  rounded-2xl max-w-screen-sm p-5 ring ring-transparent transition-shadow has-[:invalid]:ring  has-[:invalid]:ring-red-200  dark:bg-white">
-        <div className="flex flex-col flex-1 *:outline-none">
-          <input
-            className="w-full h-11 pl-4 bg-gray-300 rounded-full  
-            ring ring-transparent 
-            focus:valid:ring-green-500 focus:ring-2 focus:ring-offset-2
-            focus:invalid:ring-red-500 
-            transition-all placeholder:drop-shadow 
-            peer"
-            type="email"
-            required
-            placeholder="Email address"
-          />
-          <span className="h-6 text-red-500 font-medium pl-3 hidden peer-invalid:block">
-            Email is required
-          </span>
-        </div>
-        <button
-          className="hover:scale-90 text-white py-2 rounded-full md:px-10 active:scale-90 focus:scale-95 transition-transform font-medium  
-        bg-gradient-to-tr from-black via-gray-700 to-slate-400
-        peer-valid:bg-gradient-to-tr peer-valid:from-green-500 peer-valid:via-green-600 peer-valid:to-green-700"
-        >
-          Login
-        </button>
+      <div
+        className="bg-white w-full  shadow-lg  
+      rounded-2xl max-w-screen-sm p-5 dark:bg-white flex flex-col gap-3 "
+      >
+        {['Jonathan', 'Me', 'You', 'Mcdonalds'].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center gap-4 odd:bg-gray-300 even:bg-cyan-100 rounded-xl p-1  "
+          >
+            <div className="size-8 bg-blue-400 rounded-full" />
+            <div>
+              <span className="text-lg font-medium">{person}</span>
+            </div>
+            <div className="size-6 bg-red-500 text-white flex justify-center items-center rounded-full">
+              <span>{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
