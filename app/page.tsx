5a38e5befe9181e094f833da72daa75814a781cd
LeConsulat2 <wooine2@gmail.com>
2025-01-10 13:09:27 +1300

    Modifiers inclduing *:outline-none for children

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,31 @@
 export default function Home() {
   return (
-    <main className="bg-gray-300 h-screen flex items-center justify-center p-5">
-      <div className="bg-white w-full shadow-lg p-5 rounded-2xl">
-        <div>
-          <div className="flex justify-between items-center">
-            <div className="flex flex-col">
-              <span className="text-gray-500 font-semibold -mb-1">
-                In Transit
-              </span>
-              <span className="text-4xl font-semibold">Coolblue</span>
-            </div>
-            <div className="w-12 h-12 rounded-full bg-orange-400"></div>
-          </div>
-          <div className="my-2 flex items-center gap-2">
-            <span className="bg-green-400 text-white uppercase px-2 py-1 text-xs font-medium rounded-xl">
-              Today
-            </span>
-            <span>9:30-10:30</span>
-          </div>
-
-          <div className="relative">
-            <div className="bg-gray-200 absolute w-full h-2 rounded-full"></div>
-            <div className="bg-green-400 absolute w-1/2 h-2 rounded-full"></div>
-          </div>
-          <div className="flex justify-between items-center mt-4 text-gray-700">
-            <span>Expected</span>
-            <span>Sorting</span>
-            <span>In Transit</span>
-            <span className="text-gray-400">Delivered</span>
-          </div>
+    <main className="bg-gray-300  sm:bg-red-200 md:bg-green-200 lg:bg-cyan-300 xl:bg-slate-950 h-screen flex items-center justify-center p-5 ">
+      <div className="bg-white w-full flex flex-col flex-1 md:flex-row gap-2 shadow-lg  rounded-2xl max-w-screen-sm p-5 dark:bg-white">
+        <div className="flex flex-col flex-1 *:outline-none">
+          <input
+            className="w-full h-11 pl-4 bg-gray-300 rounded-full  
+            ring ring-transparent 
+            focus:valid:ring-green-500 focus:ring-2 focus:ring-offset-2
+            focus:invalid:ring-red-500 
+            transition-all placeholder:drop-shadow 
+            peer"
+            type="email"
+            required
+            placeholder="Email address"
+          />
+          <span className="h-6 text-red-500 font-medium hidden peer-invalid:block">
+            Email is required
+          </span>
         </div>
+
+        <button
+          className="hover:scale-90 text-white py-2 rounded-full md:px-10 active:scale-90 focus:scale-95 transition-transform font-medium  
+        bg-gradient-to-tr from-black via-gray-700 to-slate-400
+        peer-valid:bg-gradient-to-tr peer-valid:from-green-500 peer-valid:via-green-600 peer-valid:to-green-700"
+        >
+          Login
+        </button>
       </div>
     </main>
   );
