85400370f8b7e2316ae798f49b9362041419105f
LeConsulat2 <wooine2@gmail.com>
2025-01-11 19:08:43 +1300

    Log in Screen done
    Inside app folder, made create-account folder,login folder, sms folder and each hae page.tsx separate.
    
    Also how they look are shown inside components folder

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,15 +2,14 @@ import FormButton from '@/components/form-btn';
 import FormInput from '@/components/form-input';
 import SocialLogin from '@/components/social-login';
 
-export default function CreateAccount() {
+export default function Login() {
   return (
     <div className="flex flex-col gap-10 py-8 px-5">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">Kia ora!</h1>
-        <h2>Check out the Form below to explore!</h2>
+        <h2>Please log in with your email</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <FormInput type="text" placeholder="Username" required errors={[]} />
         <FormInput type="email" placeholder="Email" required errors={[]} />
         <FormInput
           type="password"
@@ -18,13 +17,8 @@ export default function CreateAccount() {
           required
           errors={[]}
         />
-        <FormInput
-          type="password"
-          placeholder="Confim password"
-          required
-          errors={[]}
-        />
-        <FormButton loading={false} text="Create Account" />
+
+        <FormButton loading={false} text="Log In" />
       </form>
       <SocialLogin />
     </div>
