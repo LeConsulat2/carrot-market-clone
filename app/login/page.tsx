157dfaf157283608fb0b361e619bd6d94a8fb09a
LeConsulat2 <wooine2@gmail.com>
2025-01-12 12:22:34 +1300

    No need for Handlers with Nextjs!

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,15 +3,27 @@ import FormInput from '@/components/form-input';
 import SocialLogin from '@/components/social-login';
 
 export default function Login() {
+  async function handleForm(formData: FormData) {
+    'use server';
+    console.log(formData.get('email'), formData.get('password'));
+  }
+
   return (
     <div className="flex flex-col gap-10 py-8 px-5">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">Kia ora!</h1>
         <h2>Please log in with your email</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
           type="password"
           placeholder="Password"
           required
