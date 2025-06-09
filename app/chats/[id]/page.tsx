62ebf66e2c2d93c506af5ebdd2e24967a6c50333
LeConsulat2 <wooine2@gmail.com>
2025-06-09 17:21:32 +1200

    Messages! (힘들어...)

--- app/chats/[id]/page.tsx
+++ app/chats/[id]/page.tsx
@@ -1,6 +1,8 @@
 import db from "@/lib/db";
 import getSession from "@/lib/session";
+import { Prisma } from "@prisma/client";
 import { notFound } from "next/navigation";
+import ChatMessagesList from "@/components/chat-messages-list";
 
 async function getRoom(id: string) {
   const room = await db.chatRoom.findUnique({
@@ -23,10 +25,38 @@ async function getRoom(id: string) {
   return room;
 }
 
+async function getMessages(chatRoomId: string) {
+    const messages = await db.message.findMany({
+        where: {
+            chatRoomId,
+        },
+        select: {
+            id: true,
+            payload: true,
+            created_at: true,
+            userId: true,
+            user: {
+                select: {
+                    username: true,
+                    avatar: true,
+                }
+            }
+        }
+    })
+    return messages;
+}
+
+export type InitialChatMessages = Prisma.PromiseReturnType<typeof getMessages>;
+
+
 export default async function ChatRoom({ params }: { params: { id: string } }) {
   const room = await getRoom(params.id);
   if (!room) {
     return notFound();
   }
-  return <h1>chat!</h1>;
+  const initialMessages = await getMessages(room.id);
+  const session = await getSession();
+  return (
+    <ChatMessagesList userId={session.id!} initialMessages={initialMessages} />
+  )
 }
