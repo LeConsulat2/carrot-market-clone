6306c932fcc06b805c386491900ad981e9cff318
LeConsulat2 <wooine2@gmail.com>
2025-06-11 16:57:39 +1200

    realtime channel

--- components/chat-messages-list.tsx
+++ components/chat-messages-list.tsx
@@ -5,15 +5,21 @@ import { InitialChatMessages } from "@/app/chats/[id]/page";
 import { formatToTimeAgo } from "@/lib/utils";
 import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0cXVzeWtybHZudHd3Zm95bm56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2MTQ4MjIsImV4cCI6MjA2NTE5MDgyMn0.ZUocUOyNlHQ31Jns6sj_yBKkHYCTcXrGuG5x4Uc_eAo";
+const SUPABASE_URL = "https://gtqusykrlvntwwfoynnz.supabase.co";
 
 interface ChatMessageListProps {
   initialMessages: InitialChatMessages;
   userId: number;
+  chatRoomId: string;
 }
 export default function ChatMessagesList({
   initialMessages,
   userId,
+  chatRoomId,
 }: ChatMessageListProps) {
   const [messages, setMessages] = useState(initialMessages);
   const [message, setMessage] = useState("");
@@ -28,6 +34,16 @@ export default function ChatMessagesList({
     alert(message);
     setMessage("");
   };
+  useEffect(() => {
+    const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY);
+    const channel = supabase.channel(`room-${chatRoomId}`);
+    channel.on("broadcast", {
+      event: "message",
+    }, (payload) => {
+      console.log("Change received:", payload);
+    });
+    channel.subscribe();
+  }, []);
   return (
     <div className="p-5 flex flex-col gap-5 min-h-screen justify-end">
       {messages.map((message) => (
